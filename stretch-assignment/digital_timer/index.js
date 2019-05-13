26d8dbadd0299c09ee756e388c96bcf5c08089ed
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-13 18:03:11 +0200

    add reset function

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,17 +1,39 @@
-const startButton = document.querySelector("button")
+const startButton = document.querySelector("#start")
+const resetButton = document.querySelector("#reset")
 startButton.addEventListener("click", checkIfActive)
+resetButton.addEventListener("click", reset)
 let active = false
+let timer;
 function checkIfActive(){
     console.log(active)
     if(active === false){
+        digits.forEach(item => item.classList.toggle("redDigit", false))
         active = true
         startTimer()
         startButton.style.backgroundColor = "grey"
         startButton.style.cursor = "not-allowed"
     }
 }
+
+function reset(){
+active = false
+clearInterval(timer)
+digits.forEach(item => item.classList.toggle("redDigit", false))
+startButton.style.backgroundColor = "green"
+startButton.style.cursor = "pointer"
+
+mscount = 0
+mhcount = 0
+secondCount = 0
+secondTensCount = 0
+msTens.textContent = "-"
+msHundreds.textContent = "-"
+secondOnes.textContent = "-"
+secondTens.textContent = "-"
+}
+
 function startTimer(){
-            let digits = (function(){setInterval(update, 10)})()
+            let start = (function(){ timer = setInterval(update, 10)})()
 }
 
 
@@ -20,6 +42,7 @@ const msTens = document.querySelector("#msTens")
 const msHundreds = document.querySelector("#msHundreds")
 const secondOnes = document.querySelector("#secondOnes")
 const secondTens = document.querySelector("#secondTens")
+const digits = document.querySelectorAll("div.digit")
 
 //miliseoncds
 let mscount = 0
@@ -37,6 +60,8 @@ function update(){
         active = false
         startButton.style.backgroundColor = "green"
         startButton.style.cursor = "pointer"
+        clearInterval(timer)
+        digits.forEach(item => item.classList.toggle("redDigit"))
         return
     }else{
         ++mscount
