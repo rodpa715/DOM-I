296b5f9f50bfb092d3d6300ed3a214639f68708d
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-13 16:33:55 +0200

    add even listener for timerQ

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,15 +1,23 @@
+const startButton = document.querySelector("#start")
+startButton.addEventListener("click", startTimer)
+function startTimer(){
+    let digits = setInterval(update, 10)
+}
 
+
+//TIMER LOGIC
 const msTens = document.querySelector("#msTens")
 const msHundreds = document.querySelector("#msHundreds")
 const secondOnes = document.querySelector("#secondOnes")
 const secondTens = document.querySelector("#secondTens")
-let digits = setInterval(update, 10)
 
+//miliseoncds
 let mscount = 0
 let mhcount = 0
-
+//seconds
 let secondCount = 0
 let secondTensCount = 0
+
 function update(){
     msTens.textContent = mscount
     msHundreds.textContent = mhcount
